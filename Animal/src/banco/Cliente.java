6252c5f126c6a48dbb0c7632b6431e8829b19e67
neguinho237 <brunomirands237@gmail.com>
2024-08-23 11:22:01 -0300

    Add files via upload

---
+++ Animal/src/banco/Cliente.java
@@ -0,0 +1,6 @@
+package banco;
+
+public class Cliente {
+    public String nome;
+    public String cpf;
+}
---
+++ Animal/src/banco/TestaBanco.java
@@ -0,0 +1,28 @@
+package banco;
+
+public class TestaBanco {
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente();
+        c1.nome = "José Carlos";
+        c1.cpf = "111.222.333-44";
+        
+        Cliente c2 = new Cliente();
+        c1.nome = "Maria Paula";
+        c1.cpf = "111.222.333-54";
+
+        ContaCorrente cont1 = new ContaCorrente();
+        cont1.nrConta = "1234-5";
+        cont1.agencia = "444";
+        cont1.titular = c1;
+
+        ContaCorrente cont2 = new ContaCorrente();
+        cont2.nrConta = "1234-5";
+        cont2.agencia = "447";
+        cont2.titular = c2;
+
+        c1.nome = "Carlos";
+        c1 = null;
+        System.err.println(cont1.titular.nome);
+        System.err.println(cont2.titular.nome);
+    }
+}
