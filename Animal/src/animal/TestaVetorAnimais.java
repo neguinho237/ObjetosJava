1475f8a1e0f66f0b311b05ac4b7ddf1e59787ac6
neguinho237 <brunomirands237@gmail.com>
2024-08-30 10:47:00 -0300

    Add files via upload

---
+++ Animal/src/animal/TestaVetorAnimais.java
@@ -0,0 +1,16 @@
+package animal;
+
+public class TestaVetorAnimais {
+    public static void main(String[] args) {
+        Cachorro [] cachorros = new Cachorro[10];
+        
+        for (int i = 0; i < cachorros.length; i++){
+            cachorros[i] = new Cachorro("Cachorro" + i, "Poodle");
+        }
+
+        for(int i = 0; i < cachorros.length; i ++){
+            cachorros[i].latir(10);
+        }
+        
+    }
+}
